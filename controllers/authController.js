de7cb842731f41c546ad4dbb4f50daf44eb859b6
rizasimkhana <riza.simkhanas@gmail.com>
2024-12-23 21:13:59 +0100

    passreset

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ async function sendResetEmail(email, randomString) {
     from: process.env.GMAIL_USER,
     to: email,
     subject: 'Password Reset Link',
-    text: `Click on the following link to reset your password: http://localhost:3004/reset-password/${randomString}`
+    text: `Click on the following link to reset your password: https://password-reset-emnq.onrender.com/reset-password/${randomString}`
   };
 
   await transporter.sendMail(mailOptions);
